b6d34be95f01e625caddc64c41553e0416fb2899
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-09-22 20:16:27 +0900

    product, cart schema 추가

---
+++ server/src/schema/cart.ts
@@ -0,0 +1,23 @@
+import { gql } from 'apollo-server-express'
+
+const cartSchema = gql`
+  type CartItem {
+    id: ID!
+    imageUrl: String!
+    price: Int!
+    title: String!
+    amount: Int!
+  }
+
+  type Query {
+    carts: [CartItem!]
+  }
+  type Mutation {
+    addCart(id: ID!): CartItem!
+    updateCart(id: ID!, amount: Int!): CartItem!
+    deleteCart(id: ID!): ID!
+    executePay(ids: [ID!]): [ID!]
+  }
+`
+
+export default cartSchema
---
+++ server/src/schema/index.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server-express'
+import productSchema from './product'
+import cartSchema from './cart'
+
+// Query 안에 각각에 스키마 파일에 존재하는 Query 애들을 묶어주는 형태라고 보면 된다
+// products: [Product!
+// product(id: ID!): Product!
+// carts: [CartItem!]
+//
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+`
+
+export default { linkSchema, productSchema, cartSchema }
