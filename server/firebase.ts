114438a7213576f3100f2c54c860b371f6045e1c
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-01-18 01:54:30 +0900

    [ADD]: Firebase 환경변수 정의

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import 'dotenv/config'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {} from 'ts-node'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.fb_apikey,
   authDomain: process.env.fb_authDomain,
@@ -13,10 +10,14 @@ const firebaseConfig = {
   storageBucket: process.env.fb_storageBucket,
   messagingSenderId: process.env.fb_messagingSenderId,
   appId: process.env.fb_appId,
+  measurementId: process.env.fb_measurementId,
 }
+
+console.log(firebaseConfig.apiKey)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
 export default app
 
 export const db = getFirestore(app)
