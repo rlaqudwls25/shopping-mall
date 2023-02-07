a2c9006ac16ed005ac9a7789d248fb774bbb0d0c
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-02-07 18:58:49 +0900

    임시커밋

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,11 @@ import { DBField, readDB } from './dbController'
     app,
     path: '/graphql',
     cors: {
-      origin: ['*'],
+      origin: [
+        '*',
+        'https://studio.apollographql.com',
+        'http://127.0.0.1:5173',
+      ],
       credentials: true,
     },
   })
