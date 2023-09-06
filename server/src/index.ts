affcc4d5681fefdeeeb5af6677589d04ab0f1a8b
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 19:38:08 +0900

    [fix]: env 파일 삭제

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,6 @@ import env from './envLoader'
         clientUrl,
         'https://studio.apollographql.com',
         'http://127.0.0.1:5173',
-        '*',
       ],
       credentials: true,
     },
