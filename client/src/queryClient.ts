083cc79686252d7b83163aa9ffec6eb771304e88
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 20:52:15 +0900

    [fix]: content-type 추가

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -23,12 +23,11 @@ export const getClient = () => {
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL as string
 
-console.log('BASE_URL', BASE_URL)
-
-console.log('BASE_URL/graphql', `${BASE_URL}/graphql`)
-
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
-  request(`${BASE_URL}/graphql`, query, variables)
+  request(`${BASE_URL}/graphql`, query, variables, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': BASE_URL,
+  })
 
 export const QueryKeys = {
   PRODUCTS: 'PRODUCTS',
