2e9150121cf9bbb9e03abc86d71b4425239ac48c
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 21:58:37 +0900

    [fix]: origin url 저추

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,6 +31,7 @@ import env from './envLoader'
         clientUrl,
         'https://studio.apollographql.com',
         'http://127.0.0.1:5173',
+        'https://shopping-mall-client-peach.vercel.app',
       ],
       credentials: true,
     },
