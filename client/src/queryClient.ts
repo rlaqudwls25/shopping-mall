d02bb5fa80157bbc519b449f59fd2ec269fcd608
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 21:19:57 +0900

    [fix]: 다시 reset

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -24,10 +24,7 @@ export const getClient = () => {
 const BASE_URL = import.meta.env.VITE_SERVER_URL as string
 
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
-  request(`${BASE_URL}/graphql`, query, variables, {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': BASE_URL,
-  })
+  request(`${BASE_URL}/graphql`, query, variables)
 
 export const QueryKeys = {
   PRODUCTS: 'PRODUCTS',
