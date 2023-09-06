874507caba84367849e3a5eb0ad92407a81a7c2c
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 21:23:44 +0900

    [fix]: cors test

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,6 +28,7 @@ import env from './envLoader'
     path: '/graphql',
     cors: {
       origin: [
+        '*',
         clientUrl,
         'https://studio.apollographql.com',
         'http://127.0.0.1:5173',
