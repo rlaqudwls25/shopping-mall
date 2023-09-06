a50683e28f82aaf31486b4ee860f6d23a58986da
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-06 20:01:56 +0900

    [fix]: baseUrl 디버깅

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -23,6 +23,10 @@ export const getClient = () => {
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL as string
 
+console.log('BASE_URL', BASE_URL)
+
+console.log('BASE_URL/graphql', `${BASE_URL}/graphql`)
+
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
   request(`${BASE_URL}/graphql`, query, variables)
 
